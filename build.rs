5e30a2724377ea9f8d7c4d05359b7f13ea0cd225
CrLF0710 <crlf0710@gmail.com>
2017-11-11 12:42:16 +0800

    Rustfmt and allow windows to build empty crate.

--- build.rs
+++ build.rs
@@ -1,18 +1,23 @@
+#![allow(unused_imports)]
 extern crate cc;
 #[macro_use]
 extern crate cfg_if;
 
-cfg_if! {
-    if #[cfg(any(target_env = "uclibc", target_env = "newlib"))] {
-        const PREDEFINITION: &'static str = "_NOT_GNU_SOURCE";
-    } else {
-        const PREDEFINITION: &'static str = "_GNU_SOURCE";
-    }
-}
-
+#[cfg(unix)]
 fn main() {
+    cfg_if! {
+        if #[cfg(any(target_env = "uclibc", target_env = "newlib"))] {
+            const PREDEFINITION: &'static str = "_NOT_GNU_SOURCE";
+        } else {
+            const PREDEFINITION: &'static str = "_GNU_SOURCE";
+        }
+    }
+
     cc::Build::new()
         .file("src/optional_const/imp.c")
         .define(PREDEFINITION, None)
         .compile("libc_spawn_helper");
 }
+
+#[cfg(not(unix))]
+fn main() {}
--- src/optional_const/mod.rs
+++ src/optional_const/mod.rs
@@ -1,11 +1,11 @@
 use libc;
 
-extern {
-    fn libc_spawn_helper_environ() -> * const * const libc::c_char;
-    fn libc_spawn_helper_try_get_posix_spawn_usevfork(val: * mut libc::c_short) -> libc::c_int;
+extern "C" {
+    fn libc_spawn_helper_environ() -> *const *const libc::c_char;
+    fn libc_spawn_helper_try_get_posix_spawn_usevfork(val: *mut libc::c_short) -> libc::c_int;
 }
 
-pub fn environ() -> * const * const libc::c_char {
+pub fn environ() -> *const *const libc::c_char {
     unsafe { libc_spawn_helper_environ() }
 }
 
